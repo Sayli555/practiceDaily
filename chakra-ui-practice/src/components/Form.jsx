a21b4b440ab4e369a52cf08457b6429199da1a5d
Sayli Giri <sayligiri555@gmail.com>
2022-09-05 11:56:17 +0530

    basic form using chakra

---
+++ chakra-ui-practice/src/components/Form.jsx
@@ -0,0 +1,45 @@
+import { Heading ,Box, Stack, Input, Button, PinInput, PinInputField, HStack,Text} from '@chakra-ui/react'
+import React, { useState } from 'react'
+
+const Form = () => {
+    const [loading,setloading]=useState(false);
+    const [pin,setPin]=useState("");
+    console.log("pin",pin)
+  return (
+    <Box >
+      <Heading>FORM</Heading>
+      <Stack gap="10px">
+        <Box>
+            <Input type="text" placeholder='email' />
+        </Box>
+        <Box>
+            <Input type="password" placeholder='password' />
+        </Box>
+        <Box>
+        <HStack>
+            <PinInput value={pin} onChange={(value)=>{
+                setPin(value)
+            }}>
+                <PinInputField />
+                <PinInputField />
+                <PinInputField />
+                <PinInputField />
+            </PinInput>
+        </HStack>
+        </Box>
+        <Text>{pin}</Text>
+        <Box>
+            <Button isLoading={loading} onClick={()=>{
+                setloading(true);
+
+                setTimeout(()=>{
+                    setloading(false)
+                },2000)
+            }}    >Submit</Button>
+        </Box>
+      </Stack>
+    </Box>
+  )
+}
+
+export default Form
