a34111d47455b32040c4abbf145fc38fa1c926c5
Sayli Giri <sayligiri555@gmail.com>
2022-09-13 16:31:39 +0530

    clauser done

--- clauser/cla.js
+++ clauser/cla.js
@@ -1,5 +1,11 @@
 //****************************** */ clause function example *********************************//
 
+// usefull for memmorization ;
+
+// application :- debouncing, throtteling
+
+
+
 // function outer(){
 //    var a=10;
 //    return function inner(b){
@@ -18,6 +24,23 @@
 // }
 
 // console.log(outer(5)(10))  // this writing techniq call currying
+
+
+
+function calculator(init=0){    // store the previous value; 
+    var value=init;
+
+    function add(val){
+        value=value+val;
+        return value
+    }
+    return {add}
+}
+
+var cal=calculator();
+
+console.log(cal.add(5));
+console.log(cal.add(5));
 
 
 
