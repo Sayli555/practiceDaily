53b131578ec503603c406b4d6ab6b51c372dff48
Sayli Giri <sayligiri555@gmail.com>
2022-09-13 16:24:09 +0530

    js practice

---
+++ clauser/pr.js
@@ -0,0 +1,25 @@
+//****************************** */ clause function example *********************************//
+
+// function outer(){
+//    var a=10;
+//    return function inner(b){
+//     return a+b
+//     }
+// }
+
+// var fn=outer();
+// console.log(fn(5));
+
+
+// function outer(a){
+//     return function inner(b){
+//         return a+b
+//     }
+// }
+
+// console.log(outer(5)(10))  // this writing techniq call currying
+
+
+
+
+
