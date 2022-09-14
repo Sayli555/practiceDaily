04104f2eaaa97a1f8c6965c5a9ddf59f649007de
Sayli Giri <sayligiri555@gmail.com>
2022-09-14 16:14:31 +0530

    toggle with clauser

--- practice/cla.js
+++ practice/cla.js
@@ -43,6 +43,26 @@
 // console.log(cal.add(5));
 
 
+// *********************************************toggle with clauser***********************************************
 
 
+function toggler(){
+    var arr=arguments;
+    var index=-1;
+    return function(){
+        index+=1;
+        if(index>=arr.length){
+            index=0
+        }
+        return arr[index]
+    }
+}
+const toggle=toggler(1,2,3);
+// const toggle=toggler("on","off");
 
+console.log(toggle());
+console.log(toggle());
+console.log(toggle());
+console.log(toggle());
+console.log(toggle());
+console.log(toggle());
