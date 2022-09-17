985fba89b3246729ccceaece2215112ffa6388d7
Sayli Giri <sayligiri555@gmail.com>
2022-09-17 12:23:19 +0530

    counter done

--- redux practice/redux-thunk/src/pages/CounterApp.jsx
+++ redux practice/redux-thunk/src/pages/CounterApp.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import * as type from "../store/action"
 
 const CounterApp = () => {
+
+  const dispatch=useDispatch();
+  const count=useSelector((state)=>state.counter.count)
   return (
     <div>
-      counter
+      <h1>counts:{count}</h1>
+      <button onClick={()=>{
+        dispatch(type.countInc())
+      }}>+</button>
+      <button
+      onClick={()=>{dispatch(type.countDec())}}
+      >-</button>
     </div>
   )
 }
--- redux practice/redux-thunk/src/pages/TodoApp.jsx
+++ redux practice/redux-thunk/src/pages/TodoApp.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import * as type from "../store/action"
 
 const TodoApp = () => {
+  const dispatch=useDispatch();
+  const [todoadd,setTodo]=useState("")
+
+  const todo=useSelector((state)=>state.todo.todos);
+
   return (
     <div>
-      todo
+      <h1>Todo</h1>
+      <input type="text" placeholder="write todos" todoadd={todoadd} onChange={()=>{
+        setTodo(value)
+      }} />
+      <button
+      onClick={()=>{
+        dispatch(type.todoAdd())
+      }}
+      >+</button>
     </div>
   )
 }
