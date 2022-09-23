9860d90c891093084a3bb88fd786f707886bf601
Sayli Giri <sayligiri555@gmail.com>
2022-09-23 17:59:20 +0530

    prototype

---
+++ practice/scripts/prototype.js
@@ -0,0 +1,12 @@
+
+let arr=["sayli","savita"];
+
+let object={
+    name:'sayli',
+    city:"pune",
+    getIntro:function(){
+        console.log(this.name +" " + this.city)
+    }
+}
+
+console.log(arr)
