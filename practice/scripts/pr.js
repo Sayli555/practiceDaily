9f00cbf455d841cfcac49e81a8a183e25a326ecb
Sayli Giri <sayligiri555@gmail.com>
2022-09-26 12:50:48 +0530

    HOF

---
+++ practice/scripts/pr.js
@@ -0,0 +1,15 @@
+let numbers = [100,200,250,450]
+
+// let final = numbers.map((num) => num + 30)
+
+var ans=numbers.map((e)=>{
+    return e*2
+})
+
+
+console.log(numbers)
+
+
+console.log(ans)
+
+
