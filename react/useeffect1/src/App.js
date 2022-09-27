347eeec6d637400aade5da2f5ddd0fe9d14ae2ae
Sayli Giri <sayligiri555@gmail.com>
2022-09-27 18:45:39 +0530

    todo done

--- react/useeffect1/src/App.js
+++ react/useeffect1/src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [newtodo,setnewtodo]=useState("");
+  console.log("newtodo",newtodo)
+  const [todos,settodos]=useState([])
+
+
+//we are using useEffect heare beacause fetch is infinite time calling 
+  useEffect(()=>{
+
+    fetch("http://localhost:8080/todos")
+    .then((r)=>r.json())
+    .then((d)=>
+    {
+      // console.log(d)  
+   settodos(d)  
+   }
+    )
+  },[])
+  
+
+  const addtodo=()=>{
+
+    fetch("http://localhost:8080/todos",{
+      method:"POST",
+      headers:{
+        "content-type":"application/json"
+      },
+      body:JSON.stringify({
+        title: newtodo,
+        iscomplited: false
+      })
+    }).then((r)=>r.json()).then((r)=>{
+      settodos([...todos,r])
+      setnewtodo("")
+    });
+
+    
+
+  };
+
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={newtodo} onChange={(e)=>
+        setnewtodo(e.target.value)
+      } />
+      <button onClick={addtodo}>ADD</button>
+      {todos.map((e)=>(
+        <div key={e.id}>{e.title}</div>
+      ))}
+    </div>
     </div>
   );
 }
