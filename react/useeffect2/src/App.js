115317451d01466cd7fd9f0c3a70195c18722421
Sayli Giri <sayligiri555@gmail.com>
2022-09-29 16:40:14 +0530

    pagination page limits

--- react/useeffect2/src/App.js
+++ react/useeffect2/src/App.js
@@ -4,18 +4,19 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [page,setpage]=useState(1)
+  const [page,setpage]=useState(1);
+  const [limit,setLimit]=useState(5);
   const [todo,setTodo]=useState([]);
   const [totalcount,settotalcount]=useState(0)
 
 
-
+console.log( typeof limit)
 
   const getTodo=async()=>{
-    axios(`http://localhost:8081/todos?_page=${page}&_limit=4`).then((r)=> {
+    axios(`http://localhost:8081/todos?_page=${page}&_limit=${limit}`).then((r)=> {
       setTodo(r.data);
       settotalcount(Number(r.headers["x-total-count"]))
-      console.log(r)
+      // console.log(r)
     });
  }
 
@@ -23,16 +24,13 @@ function App() {
 //whenever page parameter change my component is render again
   useEffect(()=>{ 
     getTodo()
-  },[page])
+  },[page,limit])
 
-console.log("todo",todo)
+// console.log("todo",todo)
 
   return (
     <div className="App">
-   {todo.map((e)=>(
-    <div key={e.id}>{e.title}</div>
-   ))}
-   <button 
+     <button 
    disabled={page<=1}
    onClick={()=>{
     if(page>=1){
@@ -40,6 +38,14 @@ console.log("todo",todo)
     }
    }}
    >{"<"}</button>
+
+   <select onChange={(e)=>setLimit(Number(e.target.value))}>
+    <option value={5}> 5</option>
+    <option value={10}> 10</option>
+    <option value={15}> 15</option>
+   </select>
+
+
    <button
    disabled={totalcount<page*4}
     onClick={()=>{
@@ -48,6 +54,10 @@ console.log("todo",todo)
       
      }}
    >{">"}</button>
+   {todo.map((e)=>(
+    <div key={e.id}>{e.title}</div>
+   ))}
+  
     </div>
   );
 }
