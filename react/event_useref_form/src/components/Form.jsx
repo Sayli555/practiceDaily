94b4bb4de62955d8b0b0370b52deb31c897d498f
Sayli Giri <sayligiri555@gmail.com>
2022-09-30 17:59:20 +0530

    form

--- react/event_useref_form/src/components/Form.jsx
+++ react/event_useref_form/src/components/Form.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react';
+import styles from "./style.module.css"
 
 const Form = () => {
+    const ref=useRef()
 
     const [form,setform]=useState({
         username:"",
@@ -13,11 +15,17 @@ const Form = () => {
 
     const onchange=(e)=>{
         console.log(e)
-        const {name,value,checked,type}=e.target;
+        const {name,value,checked,type,files}=e.target;
         if(type=== "checkbox"){
             setform({  
                 ...form,
                 [name]:checked
+            })
+        }
+        else if(type=== "file"){
+            setform({  
+                ...form,
+                [name]:files
             })
         }
         else{
@@ -26,20 +34,28 @@ const Form = () => {
                 [name]:value
             })
         }
-       
-       
-
     }
-    console.log(form)
+    
+    
+    const handlesubmit = (e) =>{
+        e.preventDefault();
+        console.log(form,"form");
+        ref.current.focus()
+        ref.current.className+=styles.red
+    }
+
+
+
 
 
   return (
     <div>
-      <form>
+      <form onSubmit={handlesubmit}>
         <div>
         <label>name</label>
             <input
             type="text"
+            ref={ref}
             placeholder='name'
             name="username"
             onChange={onchange}
@@ -91,6 +107,51 @@ const Form = () => {
            
             />
             <label>isIndian</label>
+        </div>
+        <div>
+            <div>
+                
+                <input
+                type="radio"
+                onChange={onchange}
+                name="gender"
+                value="male"
+                />
+                <lable>Male</lable>
+            </div>
+            <div>
+                
+                <input
+                type="radio"
+                onChange={onchange}
+                name="gender"
+                value="female"
+                />
+                <lable>Female</lable>
+            </div>
+            {/* **************************select tag*********************************** */}
+
+            <div>
+                <select name="selectgender" onChange={onchange} value={form.selectgender}>
+                    <option value="male">male</option>
+                    <option value="female">female</option>
+                </select>
+            </div>
+            <div>
+
+            <lable>File</lable>
+                <input
+                type="file"
+                onChange={onchange}
+                name="resume"
+                file={form.resume}
+                />
+            </div>
+        </div>
+        <div>
+            <input
+            type="submit"
+            />
         </div>
       </form>
     </div>
