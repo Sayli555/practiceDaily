73745821bbef8da0946d254fdc3f804d5c4b0c12
Sayli Giri <sayligiri555@gmail.com>
2022-10-18 21:40:59 +0530

    delete done

--- redux practice/todo_psc/src/components/TodoSingle.jsx
+++ redux practice/todo_psc/src/components/TodoSingle.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import { deleteTodoFetch, 
   // singleTodoFetch
    toggleTodoFetch } from '../redux/action';
 
 function TodoSingle() {
-  const [currentTodo,setcurrentTodo]=useState({})
+  const [currentTodo,setcurrentTodo]=useState({});
+  const navigate=useNavigate()
   const {id} =useParams();
   const dispatch=useDispatch();
   const todos=useSelector((state)=>state.todos);
   
   const deleteTodo=()=>{
-    dispatch(deleteTodoFetch(id))
+    dispatch(deleteTodoFetch(id));
+    navigate("/")
   }
 
     const toggleTodo=()=>{
