f27b12c368e248fd4f0a51e474951c4e392d0f9e
Sayli Giri <sayligiri555@gmail.com>
2022-11-02 22:21:11 +0530

    firebase set

---
+++ projects/firebase/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth"
+
+
+const app=firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIRBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIRBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIRBASE_PROJECTED_ID,
+    storageBucket: process.env.REACT_APP_FIRBASE_STOARGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIRBASE_MASSAGING_SENDING_ID,
+    appId: process.env.REACT_APP_FIRBASE_APP_ID
+})
+
+
+export const auth= app.auth()
+export default app
