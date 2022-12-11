9655a71bff2a9fd98827a15c16e2d319b3b48e05
sayali giri <sayligiri555@gmail.com>
2022-12-11 14:04:09 +0530

    asyncrus action

---
+++ backend/l1/fsasync/index.js
@@ -0,0 +1,14 @@
+const fs=require("fs");
+
+// fs.writeFile("demo.txt","add new file",(err)=>{
+//     console.log(err)
+// })
+
+// fs.appendFile("demo.txt"," exstra data add",(err)=>{
+//     console.log("append data")
+// })
+
+// fs.readFile('demo.txt',"utf8",(err,data)=>{
+//     console.log("data",data);
+//     console.log("err",err)
+// })
