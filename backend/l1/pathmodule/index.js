8c9ea7394b31732a08b3d74adfdeb5b8c4f0cf3d
sayali giri <sayligiri555@gmail.com>
2022-12-12 14:37:35 +0530

    path module

---
+++ backend/l1/pathmodule/index.js
@@ -0,0 +1,5 @@
+const path=require("path");
+console.log(path.basename('C:/Users/sayal/Documents/practicedaily/practiceDaily/backend/l1/pathmodule/index.js'))
+console.log(path.dirname('C:/Users/sayal/Documents/practicedaily/practiceDaily/backend/l1/pathmodule/index.js'))
+console.log(path.extname('C:/Users/sayal/Documents/practicedaily/practiceDaily/backend/l1/pathmodule/index.js'))
+console.log(path.parse('C:/Users/sayal/Documents/practicedaily/practiceDaily/backend/l1/pathmodule/index.js'))
