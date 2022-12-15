2f3c1988fa2dc9151566581645ddc3230c24a0a8
sayali giri <sayligiri555@gmail.com>
2022-12-15 14:30:36 +0530

    routing

--- backend/l1/http/index.js
+++ backend/l1/http/index.js
@@ -3,7 +3,17 @@
 const http=require("http");
 
 const server=http.createServer((req,res)=>{
-res.end("server")
+    if(req.url=="/"){
+        res.end("home")
+    }
+    if(req.url=="/about"){
+        res.end("about")
+    }
+    else{
+        res.writeHead(404,{"content-type":"text/html"})
+        res.end("<h1>404</h1>")
+    }
+
 })
 
 server.listen(8000,"127.0.0.1",()=>{
